5bda1ba29697b8fdbcbf11439c163181b02ed27a
vallecbg <valioplayofficial@gmail.com>
2020-07-01 21:19:26 +0300

    Feat: Created Shared folder where store common modules used all over in the project

---
+++ JS/Application Manager/AppManager/src/app/components/shared/toolbar/toolbar.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.css']
+})
+export class ToolbarComponent implements OnInit {
+  @Output() onToggleSidenav = new EventEmitter<void>();
+  isAuth: boolean;
+  userId: string;
+
+  constructor(
+  ) { }
+
+  ngOnInit() {
+    this.isAuth = false
+  }
+
+  getUserId(){
+  }
+
+  toggleSidenav(){
+    this.onToggleSidenav.emit();
+  }
+
+  logout(){
+  }
+}
