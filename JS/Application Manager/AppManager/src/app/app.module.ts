7abb84ee12f09d089d7645ad3df300423f9f79c4
vallecbg <valioplayofficial@gmail.com>
2020-07-02 20:24:55 +0300

    Feat: Created home page

--- JS/Application Manager/AppManager/src/app/app.module.ts
+++ JS/Application Manager/AppManager/src/app/app.module.ts
@@ -1,43 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common'
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { CommonModule } from "@angular/common";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { ToastrModule } from "ngx-toastr";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
-import { HttpClientModule } from '@angular/common/http'
-import { AppRoutingModule } from './app-routing.module';
-import { MaterialModule } from './material.module'
-import { FlexLayoutModule } from '@angular/flex-layout'
-import {ShareModule} from './components/shared/share/share.module'
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from "./app-routing.module";
+import { MaterialModule } from "./material.module";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { ShareModule } from "./components/shared/share/share.module";
+import { AssignmentModule } from './components/assignments/assignment.module'
 
-
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-//import home
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./components/home/home.component";
 
 //import other modules
-import { FirebaseModule } from './firebase.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     CommonModule,
     AppRoutingModule,
-    ToastrModule.forRoot({timeOut: 3000, preventDuplicates: true}),
+    ToastrModule.forRoot({ timeOut: 3000, preventDuplicates: true }),
     BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
     MaterialModule,
     FlexLayoutModule,
     ReactiveFormsModule,
-    FirebaseModule,
-    ShareModule
+    ShareModule,
+    AssignmentModule
   ],
   bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
