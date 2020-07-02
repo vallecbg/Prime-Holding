13bff0dcbe4f8c1f729de0eeb5cfca9ac9ea1a14
vallecbg <valioplayofficial@gmail.com>
2020-07-02 20:24:25 +0300

    Feat: Added page routing

--- JS/Application Manager/AppManager/src/app/app-routing.module.ts
+++ JS/Application Manager/AppManager/src/app/app-routing.module.ts
@@ -1,17 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Route, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Route, RouterModule } from "@angular/router";
 //import auth guard
 
-import {HomeComponent} from './components/home/home.component'
-
+import { HomeComponent } from "./components/home/home.component";
+import { AddAssignmentComponent } from './components/assignments/add-assignment/add-assignment.component'
+import { AssignmentDetailsComponent } from './components/assignments/assignment-details/assignment-details.component'
+import { AssignmentsListComponent } from './components/assignments/assignments-list/assignments-list.component'
 
 const routes: Route[] = [
-  {path: "", component: HomeComponent},
-  {path: "index", component: HomeComponent}
-]
+  { path: "", component: HomeComponent },
+  { path: "index", component: HomeComponent },
+  { path: "assignments", component: AssignmentsListComponent },
+  { path: "assignments/:id", component: AssignmentDetailsComponent },
+  { path: "add", component: AddAssignmentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
